e35c56b316c79341a3f867519e9d24b477ded60e
Piyushsingh400 <piyshsingh2001@gmail.com>
2023-01-30 17:47:33 +0530

    added link

--- URI.js
+++ URI.js
@@ -2,6 +2,6 @@ const isDevelopmentMode = process.env.MODE === "DEV";
 
 const clientURL = isDevelopmentMode
 	? "http://localhost:3000"
-	: ["https://adra-amie.netlify.app", "https://fb-clone-frontend.vercel.app"];
+	: "https://socialshare-i5iz.onrender.com";
 
 module.exports = { clientURL };
