d0cb4bb54b11cce3a585af5cb18f1f08694e115a
Piyushsingh400 <piyshsingh2001@gmail.com>
2023-01-30 19:01:38 +0530

    added link

--- URI.js
+++ URI.js
@@ -1,7 +1,3 @@
-const isDevelopmentMode = process.env.MODE === "DEV";
-
-const clientURL = isDevelopmentMode
-	? "http://localhost:3000"
-	: "https://socialshare-i5iz.onrender.com";
+const clientURL = "https://socialshare-i5iz.onrender.com";
 
 module.exports = { clientURL };
